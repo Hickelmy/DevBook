35291cca9fc4ce5105a96546958d0f9962bd0ff3
Riquelmy Silva <riquelmy.silva@grupoicts.com.br>
2022-06-13 02:24:40 -0400

    feat: adicionando estrutura inicial e estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import appcss from "./app.css";
+import styles from "./app.module.css";
 
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Feed } from "./components/Feed";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <h1>Algum texto aqui</h1>
+        </main>
+      </div>
     </div>
   );
 }
